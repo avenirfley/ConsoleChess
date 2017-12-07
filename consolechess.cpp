34a2c0a9d31c2163031131497ed30239304cc624
Avenir Fleyshgauer <avenirfley@gmail.com>
2017-12-07 08:20:12 -0800

    clear screen and basic move code
    resets the screen every move.
    swaps characters for a move currently

--- consolechess.cpp
+++ consolechess.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
+#include <stdio.h>
 #include <string>
+#include <string.h>
 #include <fstream>
 #include <array>
 #include <algorithm>
@@ -29,9 +31,9 @@ print "thank you for playing the game";
 *************************************/
 
 
-array<char*, 8> DEFAULT_POSITION = {"rnbqkbnr","pppppppp", "OOOOOOOO","OOOOOOOO","OOOOOOOO","OOOOOOOO","PPPPPPPP","RNBQKBNR"};
+char DEFAULT_POSITION[64];
 
-void DisplayBoard(array<char*, 8> temp)
+void DisplayBoard(char* temp)
 {
     cout <<"    1  2  3  4  5  6  7  8 "<<endl;
     cout <<"  -------------------------"<<endl;
@@ -40,41 +42,27 @@ void DisplayBoard(array<char*, 8> temp)
         cout<<((char)('a'+i))<<"| ";
         for(int j=0;j<8;j++)
         {
-            cout<<" "<<temp[i][j]<<" ";
+            cout<<" "<<temp[i*8+j]<<" ";
         }
         cout<<"|"<<endl;
     }
     cout <<"  -------------------------"<<endl;
 }
 
-array<char*, 8> movePiece(array<char*, 8> temp,string source,string dest)
+void consolechess(char* tempBoard)
 {
-    /*int Row = (sourceRow-'a');
-    int destiRow = (destRow-'a');
-    char temp2 = temp[Row][sourceCol];
-    char temp3 = temp[destiRow][destCol];
-    temp[Row][sourceCol] = temp3;
-    temp[destiRow][destCol] = temp2;*/
-    return temp;
-}
-
-void consolechess(array<char*, 8> tempBoard)
-{
-    array<char*, 8> currentBoard = tempBoard;
-
-    DisplayBoard(currentBoard);
-
-    string BLACK = "black";
-    string WHITE = "white";
-    string EMPTY = "[]";
-    string PAWN = "pawn";
-    string KNIGHT = "knight";
-    string BISHOP = "bishop";
-    string ROOK = "rook";
-    string QUEEN = "queen";
-    string KING = "king";
+    char* currentBoard = tempBoard;
+    char BLACK = 'b';
+    char WHITE = 'w';
+    char EMPTY = ('a'-1);
+    char PAWN = 'p';
+    char KNIGHT = 'n';
+    char BISHOP = 'b';
+    char ROOK = 'r';
+    char QUEEN = 'q';
+    char KING = 'k';
     string toMove = "";
-    string turn = WHITE;
+    char turn = WHITE;
     string temp;
     string unitTemp, unitTemp2;
 
@@ -84,9 +72,10 @@ void consolechess(array<char*, 8> tempBoard)
     int whiteScore = 0;
     int blackScore = 0;
 
+    cout<<"Turn: "<<((turn==WHITE)?"WHITE":"BLACK")<<endl;
     while(!bExit)
     {
-        cout<<"Turn: "<<((turn==WHITE)?"WHITE":"BLACK")<<endl;
+        DisplayBoard(currentBoard);
         bValid = false;
         while(!bValid)
         {
@@ -98,23 +87,34 @@ void consolechess(array<char*, 8> tempBoard)
             transform(unitTemp2.begin(), unitTemp2.end(), unitTemp2.begin(), ::tolower);
             // Parse letter and number to process here...
 
-            if(unitTemp2.empty() || unitTemp.empty() || (unitTemp.length() > 2) || (unitTemp2.length() > 2)) {
+            if(unitTemp2.empty() || unitTemp.empty() || (unitTemp.length() > 2) ||
+               (unitTemp2.length() > 2) || (((unitTemp.c_str()[0])-'a') > 7) ||
+               (((unitTemp2.c_str()[0])-'a') > 7) || (((unitTemp.c_str()[1])-'0')-1 > 7) ||
+               (((unitTemp2.c_str()[1])-'0')-1 > 7)) {
                 cout<<"You entered an invalid action!"<<endl;
                 bValid = false;
             }
             else {
-                cout<<endl<<"Attempting to move "<<turn<<" "<<unitTemp<<" to "<< unitTemp2<<endl;
 
-                currentBoard = movePiece(currentBoard, unitTemp,unitTemp2);
 
-                DisplayBoard(currentBoard);
+                char sourceRow = unitTemp.c_str()[0];
+                char destRow = unitTemp2.c_str()[0];
+                int sourceCol = ((unitTemp.c_str()[1])-'0')-1;
+                int destCol = ((unitTemp2.c_str()[1])-'0')-1;
+                int Row = (sourceRow-'a');
+                int destiRow = (destRow-'a');
+                char temp2 = currentBoard[(Row*8)+sourceCol];
+                char temp3 = currentBoard[(destiRow*8)+destCol];
+                currentBoard[(Row*8)+sourceCol] = temp3;
+                currentBoard[(destiRow*8)+destCol] = temp2;
+                system("cls");
+
+                cout<<((turn==WHITE)?"WHITE":"BLACK")<<"moved "<<unitTemp<<" to "<<unitTemp2<<endl;
 
                 if(turn == WHITE) whiteScore+=10;
                 else blackScore+=10;
             }
         }
-
-        cout<<"-----   EndTurn   -----"<<endl<<endl;
 
         //switch the turn
         if(turn == WHITE) turn = BLACK;
@@ -151,9 +151,7 @@ void consolechess(array<char*, 8> tempBoard)
 
 int main()
 {
-    cout<<"Initializing Console Chess..."<<endl;
-
+    strcpy(DEFAULT_POSITION,"rnbqkbnrppppppppOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOPPPPPPPPRNBQKBNR");
     consolechess(DEFAULT_POSITION);
-
     return 1;
 }
