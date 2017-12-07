b260e18d19b9d05935cb0b5bbbe3c7e3d57aff61
Avenir Fleyshgauer <avenirfley@gmail.com>
2017-12-07 05:37:20 -0800

    started move to classic gameloop
    Erased the menu on each move
    put the move into a single statement
    using only letter number for  input, will need more validation.
    have it loop through turns.
    marks end  of turn

--- consolechess.cpp
+++ consolechess.cpp
@@ -7,6 +7,7 @@
 using namespace std;
 
 // THE CLASSIC GAMME LOOP
+// NOTES: Switch array of c strings to one fen array. (fen is a chess programming term)
 /**********************************
  state := playGame;
 initialize position
@@ -46,7 +47,7 @@ void DisplayBoard(array<char*, 8> temp)
     cout <<"  -------------------------"<<endl;
 }
 
-array<char*, 8> movePiece(array<char*, 8> temp, char sourceRow,int sourceCol,char destRow,int destCol)
+array<char*, 8> movePiece(array<char*, 8> temp,string source,string dest)
 {
     /*int Row = (sourceRow-'a');
     int destiRow = (destRow-'a');
@@ -75,7 +76,7 @@ void consolechess(array<char*, 8> tempBoard)
     string toMove = "";
     string turn = WHITE;
     string temp;
-    string unitTemp;
+    string unitTemp, unitTemp2;
 
     bool bExit = false;
     bool bValid = false;
@@ -86,92 +87,65 @@ void consolechess(array<char*, 8> tempBoard)
     while(!bExit)
     {
         cout<<"Turn: "<<((turn==WHITE)?"WHITE":"BLACK")<<endl;
-        cout<<"What would you like to do? ('exit', 'move', 'endgame')"<<endl;
-        cin>>temp;
-        transform(temp.begin(), temp.end(), temp.begin(), ::tolower);
-        if(temp.compare("exit") == 0){
-            bExit=true;
-        }
-        else if(temp.compare("move") == 0){
-            bValid = false;
-            while(!bValid)
-            {
-                bValid = true;
-                cout<<"What unit would you like to move?";
-                cin>>unitTemp;
-                transform(unitTemp.begin(), unitTemp.end(), unitTemp.begin(), ::tolower);
-                if(unitTemp.compare("pawn") == 0) toMove = PAWN;
-                else if(unitTemp.compare("bishop") == 0) toMove = BISHOP;
-                else if(unitTemp.compare("knight") == 0) toMove = KNIGHT;
-                else if((unitTemp.compare("rook") == 0) | (unitTemp.compare("tower") == 0)) toMove = ROOK;
-                else if(unitTemp.compare("king") == 0) toMove = KING;
-                else if(unitTemp.compare("queen") == 0) toMove = QUEEN;
-                else {
-                    bValid = false;
-                    cout << "Please enter a valid piece name!"<<endl;
-                    cout<<"(pawn,bishop,knight,rook,king,queen): ";
-                }
-            }
-            bValid = false;
-            while(!bValid)
-            {
-                bValid = true;
-                cout<<"Where do you want to move? 'letternumber' (e4): ";
-                cin>>unitTemp;
-                transform(unitTemp.begin(), unitTemp.end(), unitTemp.begin(), ::tolower);
-                // Parse letter and number to process here...
-
-                if(unitTemp.length() > 2) {
-                    cout<<"You entered an invalid destination!"<<endl;
-                    bValid = false;
-                }
-                else {
-                    cout<<endl<<"Attempting to move "<<turn<<" "<<toMove<<" to "<< unitTemp<<endl;
-
-                    currentBoard = movePiece(currentBoard, 'g',4,'e',4);
-
-                    DisplayBoard(currentBoard);
-
-                    if(turn == WHITE) whiteScore+=10;
-                    else blackScore+=10;
-                }
-            }
-
-            //switch the turn
-            if(turn == WHITE) turn = BLACK;
-            else turn = WHITE;
-        }
-        else if(temp.compare("endgame")==0) {
-            cout<<endl<<"------ GAME OVER ------"<<endl;
-            cout<<"White Score: "<< whiteScore<<endl;
-            cout<<"Black Score: "<< blackScore<<endl;
-            cout<<endl<<"Would you like to start a new game? (yes/no): ";
-            cin>>unitTemp;
-            transform(unitTemp.begin(), unitTemp.end(), unitTemp.begin(), ::tolower);
-            bValid = false;
-            while(!bValid)
-            {
-                bValid = true;
-                if(unitTemp.compare("yes") == 0) {
-                    cout<<endl<<"Starting a new game..."<<endl;
-                    currentBoard = DEFAULT_POSITION;
-                    whiteScore = blackScore = 0;
-                    turn = WHITE;
-                    toMove="";
-                    consolechess(DEFAULT_POSITION);
-                    bExit = true;
-                }
-                else if (unitTemp.compare("no") == 0) bExit = true;
-                else {
-                    cout<<endl<<"Please Enter yes/no: ";
-                    bValid = false;
-                }
-            }
-        }
-        else
+        bValid = false;
+        while(!bValid)
         {
-            cout<<endl<<"Invalid action entered!"<<endl;
+            bValid = true;
+            cout<<"Please enter action with 'letternumber' Example: g4 e4 "<<endl;
+            cin>>unitTemp;
+            cin>>unitTemp2;
+            transform(unitTemp.begin(), unitTemp.end(), unitTemp.begin(), ::tolower);
+            transform(unitTemp2.begin(), unitTemp2.end(), unitTemp2.begin(), ::tolower);
+            // Parse letter and number to process here...
+
+            if(unitTemp2.empty() || unitTemp.empty() || (unitTemp.length() > 2) || (unitTemp2.length() > 2)) {
+                cout<<"You entered an invalid action!"<<endl;
+                bValid = false;
+            }
+            else {
+                cout<<endl<<"Attempting to move "<<turn<<" "<<unitTemp<<" to "<< unitTemp2<<endl;
+
+                currentBoard = movePiece(currentBoard, unitTemp,unitTemp2);
+
+                DisplayBoard(currentBoard);
+
+                if(turn == WHITE) whiteScore+=10;
+                else blackScore+=10;
+            }
         }
+
+        cout<<"-----   EndTurn   -----"<<endl<<endl;
+
+        //switch the turn
+        if(turn == WHITE) turn = BLACK;
+        else turn = WHITE;
+//        if(temp.compare("endgame")==0) {
+//            cout<<endl<<"------ GAME OVER ------"<<endl;
+//            cout<<"White Score: "<< whiteScore<<endl;
+//            cout<<"Black Score: "<< blackScore<<endl;
+//            cout<<endl<<"Would you like to start a new game? (yes/no): ";
+//            cin>>unitTemp;
+//            transform(unitTemp.begin(), unitTemp.end(), unitTemp.begin(), ::tolower);
+//            bValid = false;
+//            while(!bValid)
+//            {
+//                bValid = true;
+//                if(unitTemp.compare("yes") == 0) {
+//                    cout<<endl<<"Starting a new game..."<<endl;
+//                    currentBoard = DEFAULT_POSITION;
+//                    whiteScore = blackScore = 0;
+//                    turn = WHITE;
+//                    toMove="";
+//                    consolechess(DEFAULT_POSITION);
+//                    bExit = true;
+//                }
+//                else if (unitTemp.compare("no") == 0) bExit = true;
+//                else {
+//                    cout<<endl<<"Please Enter yes/no: ";
+//                    bValid = false;
+//                }
+//            }
+//        }
     }
 }
 
